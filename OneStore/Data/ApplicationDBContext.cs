46bb9300252962e96b699ae0f3ff28704fefc029
Alexey <84779107+razenxc@users.noreply.github.com>
2025-01-26 14:45:06 +0200

    Add project files.

---
+++ OneStore/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneStore.Models;
+
+namespace OneStore.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ OneStore/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OneStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2))")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18, 2))")]
+        public decimal DiscountPrice { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
